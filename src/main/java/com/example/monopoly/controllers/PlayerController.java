8a3427770b13b0b48b246716d6e783080db8054f
viowla <55015538+viowla@users.noreply.github.com>
2020-05-02 15:42:07 +0600

    change file

--- src/main/java/com/example/monopoly/controllers/PlayerController.java
+++ src/main/java/com/example/monopoly/controllers/PlayerController.java
@@ -1,5 +1,20 @@
 package com.example.monopoly.controllers;
 
+
+import com.example.monopoly.models.roles.Player;
+import com.example.monopoly.repositories.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/player")
 public class PlayerController {
+    @Autowired
+    private PlayerRepository playerRepository;
+
+    @GetMapping("/")
+    public Player getAllPlayers(){}
 
 }
