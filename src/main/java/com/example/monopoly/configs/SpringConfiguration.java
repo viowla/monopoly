d05a307e80faa235923ca8a0b7397fc034cf0fb7
viowla <55015538+viowla@users.noreply.github.com>
2020-05-04 10:51:30 +0600

    change file

--- src/main/java/com/example/monopoly/configs/SpringConfiguration.java
+++ src/main/java/com/example/monopoly/configs/SpringConfiguration.java
@@ -19,7 +19,7 @@ public class SpringConfiguration {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("kz.iitu.monopoly"))
+                .apis(RequestHandlerSelectors.basePackage("com.example.monopoly"))
                 .paths(PathSelectors.ant("/api/*"))
                 .build();
     }
