bb5b04be14d33cf13f200bb142e649342732ae24
viowla <55015538+viowla@users.noreply.github.com>
2020-05-04 11:52:46 +0600

    change file

--- src/main/java/com/example/monopoly/exceptions/FreeRentException.java
+++ src/main/java/com/example/monopoly/exceptions/FreeRentException.java
@@ -3,6 +3,7 @@ package com.example.monopoly.exceptions;
 import com.example.monopoly.services.exceptionService.EconomicService;
 
 public class FreeRentException extends EconomicService {
+
     public FreeRentException(){
         super("User can't give free rent!");
     }
