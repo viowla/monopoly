a2371d37d32f2543918d28ab8e893fc2d0e80319
viowla <55015538+viowla@users.noreply.github.com>
2020-05-20 15:11:04 +0600

    add Board controller and repository edit player controller

---
+++ src/main/java/com/example/monopoly/controllers/BoardController.java
@@ -0,0 +1,4 @@
+package com.example.monopoly.controllers;
+
+public class BoardController {
+}
--- src/main/java/com/example/monopoly/controllers/PlayerController.java
+++ src/main/java/com/example/monopoly/controllers/PlayerController.java
@@ -16,19 +16,13 @@ public class PlayerController {
     @Autowired
     private PlayerRepository playerRepository;
 
-    @ApiOperation(response = {
-            @ApiResponse(code = 404, message = "Player is not found"),
-            @ApiResponse(code = 400, message = "Invalid player")
-    })
+
     @GetMapping("/")
     public List<Player> getAllPlayers(){
         return playerRepository.findAll();
     }
 
-    @ApiOperation(response = {
-            @ApiResponse(code = 404, message = "Player is not found"),
-            @ApiResponse(code = 400, message = "Invalid player")
-    })
+
     @GetMapping("/{id}")
     public List<Player> findPlayerById(@PathVariable("id") Long id){
         return (List<Player>) playerRepository.findById(id).get();
@@ -42,10 +36,6 @@ public class PlayerController {
         playerRepository.deleteById(id);
     }
 
-    @ApiOperation(response = {
-            @ApiResponse(code = 404, message = "Player is not changed"),
-            @ApiResponse(code = 400, message = "Invalid player")
-    })
     @PutMapping("/{id}")
     public Player updatePlayer(@PathVariable("id") Long id,
                                @RequestBody Player player){
@@ -53,10 +43,7 @@ public class PlayerController {
         return  playerRepository.saveAndFlush(player);
     }
 
-    @ApiOperation(response = {
-            @ApiResponse(code = 404, message = "Player is not created"),
-            @ApiResponse(code = 400, message = "Invalid player")
-    })
+
     @PostMapping("")
     public Player createPlayer(@RequestBody Player player){
         return playerRepository.saveAndFlush(player);
