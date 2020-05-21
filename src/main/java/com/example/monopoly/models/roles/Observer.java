4a6a358b95fe8d76f1b06bd8f8b82788278c0272
viowla <55015538+viowla@users.noreply.github.com>
2020-05-21 07:00:19 +0600

    add user details to Observer

--- src/main/java/com/example/monopoly/models/roles/Observer.java
+++ src/main/java/com/example/monopoly/models/roles/Observer.java
@@ -1,9 +1,60 @@
 package com.example.monopoly.models.roles;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-public class Observer {
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+public class Observer implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")}
+    )
+    private List<Roles> roles;
+
 
     public static void print(Player player, String msg) {
         System.out.println("[Turn " + (player.getTotalWalk()+ 1) + "] [" + player.getPosition() + "] [$" + player.getMoney() + "] " + msg);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 }
