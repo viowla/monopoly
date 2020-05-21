f7e99c9ea318ef9c631f2d2e9c3da23fd146e858
viowla <55015538+viowla@users.noreply.github.com>
2020-05-21 06:40:28 +0600

    add @AllArgsConstructor

--- src/main/java/com/example/monopoly/models/squares/Square.java
+++ src/main/java/com/example/monopoly/models/squares/Square.java
@@ -2,22 +2,14 @@ package com.example.monopoly.models.squares;
 
 import com.example.monopoly.models.Board;
 import com.example.monopoly.models.roles.Player;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public abstract class Square {
     String name;
 
-    public Square(String name) {
-        this.name = name;
-    }
-
-
-
     public abstract void doAction(Player player, Board board);
 
-    /*----------------------------*/
-    /*public String getName() {
-        return name;
-    }*/
 }
